9a3a59e5e1fda4a2f6c5d34b00d2a0f97df7db94
ouail-nazim <ouailnazim20@gmail.com>
2020-11-09 21:51:07 +0100

    part 2 - home page

--- scripts/boxhover.js
+++ scripts/boxhover.js
@@ -6,3 +6,7 @@ const hide=element=>{
     element.style.opacity=0;
     element.previousSibling.previousElementSibling.style.opacity=1;
 }
+
+const showlist=element=>{
+    console.log(element.childNodes[3].classList.toggle('hide'));
+}
